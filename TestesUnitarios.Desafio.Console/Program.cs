eeca7d8b123f36fb89308282673078df6eead6b9
ThiagoAtaide17 <thiagoataidegoncalves@gmail.com>
2024-09-10 22:10:42 -0300

    ProgramAlterado!

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TestesUnitarios.Desafio.Console.Services;
+
 Console.WriteLine("Hello, World!");
+
+ValidacoesLista x1 = new ValidacoesLista();
+ var valores = new List<int> {5,-5,-9,8};
+
+valores.Add(-7);
+
+
+valores = x1.RemoverNumerosNegativos(valores);
+
+for (int i = 0; i < valores.Count; i++)
+{
+    Console.WriteLine(valores[i]);
+}
