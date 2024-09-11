dd3948ee8c2900845512bd7455738b04fdbce79c
ThiagoAtaide17 <thiagoataidegoncalves@gmail.com>
2024-09-10 22:11:32 -0300

    ValidacaoListaConcluido!

--- TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -2,9 +2,11 @@ namespace TestesUnitarios.Desafio.Console.Services
 {
     public class ValidacoesLista
     {
-        public List<int> RemoverNumerosNegativos(List<int> lista)
+
+          public List<int> RemoverNumerosNegativos(List<int> lista)
         {
             var listaSemNegativos = lista.Where(x => x > 0);
+            
             return listaSemNegativos.ToList();
         }
 
